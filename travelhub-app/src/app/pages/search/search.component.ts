0688f5c2696b3c79d558269940d2dde27eebd5d5
Eduard Bernal <e.bernalo@uniandes.edu.co>
2026-04-11 18:58:13 -0500

    Correcciones lint

--- travelhub-app/src/app/pages/search/search.component.ts
+++ travelhub-app/src/app/pages/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, computed } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
